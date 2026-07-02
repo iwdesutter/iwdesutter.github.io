38c2c23caef6e7b6ed21ed2eba5bfeeeb0b83a91
Isaac DeSutter <isaacdesutter@gmail.com>
2026-07-02 11:26:27 -0500

    first commit

---
+++ scripts/layout_defaults.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+  // document.getElementById("navbar").innerHTML = '<a href="">Home</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="about/index.html">About</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="work/index.html">Work</a>';
+  document.getElementById("footer").innerHTML = 'Isaac DeSutter &copy; 2026';
+} 
